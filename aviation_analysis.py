e61eec072f3075ffbd5d576cb4804a33eabbebe3
jrsswish <quodet.johnrey@gmail.com>
2026-03-31 14:02:17 -0600

    implemented the departure analysis

--- aviation_analysis.py
+++ aviation_analysis.py
@@ -10,15 +10,26 @@ def open_csv(x):
 
 def country_analysis(x):
     df = open_csv(x)
-    state_most = df["STATE_NAME"].value_counts().head().values
-    state_most_index = df["STATE_NAME"].value_counts().head().index
+    state_most = df["STATE_NAME"].value_counts().sort_values(ascending=False).head()
+    state_most_index = state_most.index
     plt.bar(state_most_index, state_most)
+
     plt.xlabel("Country")
     plt.ylabel("Delay Count")
     plt.title("The top 5 countries with the most delays")
     plt.show()
 
-    print(state_most)
+def departure_analysis(x):
+    df = open_csv(x)
+    departure_most = df.groupby("STATE_NAME")["FLT_DEP_1"].sum().sort_values(ascending=False).head()
+    departure_most_index = departure_most.index
+
+    plt.bar(departure_most_index, departure_most)
+    plt.xlabel("Departure Time")
+    plt.ylabel("Departure Count")
+    plt.title("The top 5 departure times with the most delays")
+    plt.show()
+
 if __name__ == '__main__':
     input1 = sys.argv[1]
-    country_analysis(input1)
+    departure_analysis(input1)
