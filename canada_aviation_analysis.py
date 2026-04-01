7c17d806d515e125f958bdd864a537b42e386bce
jrsswish <quodet.johnrey@gmail.com>
2026-03-31 21:57:53 -0600

    download stopwords from nltk as well as sentence tokenize the sentences on the summary column.

--- canada_aviation_analysis.py
+++ canada_aviation_analysis.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import nltk
 from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
 
 # word tokenize: breaks down the text into words
 # sent tokenize: breaks down the text into sentences
@@ -33,10 +35,18 @@ def occurence_type_analysis(x):
     plt.title("Occurence Type that occurs the most")
     plt.show()
 
+def occurence_sentence_analysis(x):
+    df = open_csv(x)
+    df = pd.DataFrame(df)
+    summary_sentences = df["Summary"].dropna().apply(sent_tokenize)
+    stop_words = set(stopwords.words('english'))
+    print(summary_sentences)
+    print(stop_words)
 
 
 
 if __name__ == '__main__':
     input1 = sys.argv[1]
     # location_analysis(input1)
-    occurence_type_analysis(input1)
+    # occurence_type_analysis(input1)
+    occurence_sentence_analysis(input1)
