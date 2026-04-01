b82fdbc30fc5b3b6e05558c78573365eb71af40f
jrsswish <quodet.johnrey@gmail.com>
2026-03-31 20:55:56 -0600

    import natural language tool kit (nltk)

--- canada_aviation_analysis.py
+++ canada_aviation_analysis.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+from nltk import word_tokenize, sent_tokenize
+
+# word tokenize: breaks down the text into words
+# sent tokenize: breaks down the text into sentences
 
 def open_csv(x):
     df = pd.read_csv(x)
@@ -19,7 +23,20 @@ def location_analysis(x):
     plt.title("The top 5 location in Canada with the most occurences")
     plt.show()
 
+def occurence_type_analysis(x):
+    df = open_csv(x)
+    df = pd.DataFrame(df)
+    occur_type_most = df["OccIncidentTypeID_DisplayEng"].value_counts().sort_values(ascending=False).head()
+    plt.bar(occur_type_most.index, occur_type_most.values)
+    plt.xlabel("Occurence Type")
+    plt.ylabel("Occurence Count")
+    plt.title("Occurence Type that occurs the most")
+    plt.show()
+
+
+
 
 if __name__ == '__main__':
     input1 = sys.argv[1]
-    location_analysis(input1)
+    # location_analysis(input1)
+    occurence_type_analysis(input1)
